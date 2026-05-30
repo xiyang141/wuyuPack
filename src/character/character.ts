ffa14ca7c910aefd724003d8a8986d713ba714a7
xiyang141 <3495404507@qq.com>
2026-05-30 19:00:16 +0800

    update

--- src/character/character.ts
+++ src/character/character.ts
@@ -1,25 +1,4 @@
 export const character = {
-	wu_zhangfei: {
-		sex: "male",
-		hp: 4,
-		group: "shu",
-		skills: ["zisheng", "xianlue", "haoxian"],
-		info: {
-			title: "烈武桓威",
-			pack: "xianding",
-			sort: "sp2_wumiao",
-		},
-	},
-	niutoumamian: {
-		sex: "male",
-		hp: 4,
-		group: "shen",
-		skills: ["dianbu", "juhun"],
-		info: {
-			pack: "collab",
-			sort: "collab_oldoudizhu",
-		},
-	},
 	huamulan: {
 		sex: "female",
 		hp: 4,
@@ -52,102 +31,6 @@ export const character = {
 			sort: "sp2_mouding",
 		},
 	},
-	dc_sb_guanyu: {
-		sex: "male",
-		hp: 4,
-		group: "shu",
-		skills: ["guanwu", "weishi", "jvao"],
-		info: {
-			tilte: "单刀赴会",
-			pack: "xianding",
-			sort: "sp2_mouding",
-		},
-	},
-	dengwantang: {
-		sex: "female",
-		hp: 3,
-		group: "wei",
-		skills: ["daijia", "chengchong"],
-		info: {
-			title: "黛流宫传",
-			pack: "xianding",
-			sort: "sp2_jinse",
-		},
-	},
-	luwenyi: {
-		sex: "female",
-		hp: 3,
-		group: "wu",
-		skills: ["caiyun", "qieyan"],
-		info: {
-			title: "卷中避世",
-			pack: "xianding",
-			sort: "sp2_jinse",
-		},
-	},
-	ol_zhengxuan: {
-		sex: "male",
-		hp: 3,
-		group: "qun",
-		skills: ["shixing", "dejiao"],
-		info: {
-			title: "弘道传德",
-			pack: "sp",
-			sort: "sp_sibi",
-		},
-	},
-	tangtang: {
-		sex: "female",
-		hp: 3,
-		group: "qun",
-		skills: ["zexing", "zhiyi"],
-		info: {
-			title: "伊侍佐佑",
-			pack: "sp",
-			sort: "sp_nvshi",
-		},
-	},
-	lizhaoyi: {
-		sex: "female",
-		hp: 3,
-		group: "shu",
-		skills: ["dcmingjie", "dcxianfu"],
-		info: {
-			title: "贞骨陨玉",
-			pack: "xianding",
-			sort: "sp2_jinse",
-		},
-	},
-	ol_sb_zhurong: {
-		sex: "female",
-		hp: 4,
-		group: "shu",
-		skills: ["olsbrenche", "olsbyalian"],
-		info: {
-			pack: "onlyOL",
-			sort: "onlyOL_sb_huahao",
-		},
-	},
-	ol_sb_tianfeng: {
-		sex: "male",
-		hp: 2,
-		group: "qun",
-		skills: ["olsbzhijian", "olsbxiaojie"],
-		info: {
-			pack: "onlyOL",
-			sort: "onlyOL_sb_jichu",
-		},
-	},
-	ol_duanwei: {
-		sex: "male",
-		hp: 4,
-		group: "qun",
-		skills: ["taohuai"],
-		info: {
-			pack: "sp",
-			sort: "sp_tianzhu",
-		},
-	},
 	clan_luyusheng: {
 		sex: "female",
 		group: "wu",
@@ -159,48 +42,6 @@ export const character = {
 			sort: "clan_lu",
 		},
 	},
-	ol_sunhanhua: {
-		sex: "female",
-		hp: 3,
-		group: "wu",
-		skills: ["oldangmo", "oljihui", "olxiaju"],
-		info: {
-			pack: "onlyOL",
-			sort: "onlyOL_waitingforsort",
-		},
-	},
-	ylyg_zhurong: {
-		sex: "female",
-		group: "shu",
-		hp: 4,
-		skills: ["ylyglieren"],
-		info: {
-			title: "刺美人",
-			pack: "offline",
-			sort: "offline_yanling",
-		},
-	},
-	ylyg_xuhuang: {
-		sex: "female",
-		group: "wei",
-		hp: 4,
-		skills: ["ylygduanliang", "ylygzier"],
-		info: {
-			title: "公明大义",
-			pack: "offline",
-			sort: "offline_yanling",
-		},
-	},
-	dc_muludawang: {
-		sex: "male",
-		hp: 6,
-		group: "qun",
-		skills: ["dczhoufa", "dcshouqun"],
-		info: {
-			pack: "huicui",
-			sort: "sp_yijun",
-		},
-	},
 	dc_zhanggong: {
 		sex: "male",
 		hp: 3,
@@ -209,17 +50,6 @@ export const character = {
 		info: {
 			pack: "sp2",
 			sort: "sp2_zzltx",
-		},
-	},
-	v_mateng: {
-		sex: "male",
-		hp: 5,
-		group: "qun",
-		skills: ["dcheji", "dchuirui", "dcxiaohui"],
-		info: {
-			title: "千骑卷黄沙",
-			pack: "xianding",
-			sort: "sp2_junwei",
 		},
 	},
 };
