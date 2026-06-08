6438b95dccb9fde47414df6fa416e16d5b6e185c
xiyang141 <3495404507@qq.com>
2026-06-08 19:07:12 +0800

    update

--- src/sweet/skill.ts
+++ src/sweet/skill.ts
@@ -12,18 +12,19 @@ export const skills: {
 		forced: true,
 		charlotte: true,
 		filter(event, player) {
-			return event.closeDialog != false && player == game.me && event.player == player && _status.gameStarted;
-		},
-		async content(event, trigger, player) {
-			let dialog = trigger.dialog;
-			if (!dialog) {
-				dialog = ui.create.dialog.apply(null, trigger.createDialog);
+			if (event.closeDialog || player != game.me || event.player != player || !_status.gameStarted) {
+				return false;
+			}
+			let dialog = event.dialog;
+			let allCard = true;
+			let hasBtn = false;
+			if (!dialog && event.createDialog.length > 0) {
+				dialog = ui.create.dialog.apply(null, event.createDialog);
 			}
 			if (dialog) {
 				const list = [];
-				let allCard = true;
-				let hasBtn = false;
-				Array.from(dialog.querySelectorAll(".buttons")).forEach(el => {
+				Array.from(dialog.querySelectorAll(".buttons"));
+				for (const el of list) {
 					if (!hasBtn) {
 						hasBtn = true;
 					}
@@ -32,42 +33,54 @@ export const skills: {
 						list.addArray(buttons);
 					} else {
 						allCard = false;
+						break;
 					}
-				});
-				if (allCard && hasBtn) {
-					const caption = dialog.querySelector(".caption")?.textContent || "";
-					dialog.close();
-					const description = dialog.querySelector(".text")?.textContent || "";
-					trigger.dialog = ui.create.dialog(caption + "\n" + description);
-					const cards2 = player.getCards("hs").forEach(c => c.classList.add("hidden", "wyremoving"));
-					const cards = list.map((btn, i) => {
-						const link = btn.link;
-						if (Array.isArray(link)) {
-							const card = game.createCard(link[2], "", "", link[3]);
-							card.node.suitnum.remove();
-							card.storage.link = link;
-							return card;
-						} else {
-							const owner = get.owner(link);
-							const cardInfo = get.cardInfo(link);
-							const card = game.createCard(cardInfo[2], cardInfo[0], cardInfo[1], cardInfo[3]);
-							card.storage.link = link;
-							if (owner) {
-								ui.create.div(".gaintag", get.translation(owner), card);
-							}
-							if (btn.classList.contains("blank")) {
-								card.querySelector(".image").remove();
-								card.style.backgroundImage = "var(--cardback-url)";
-							}
-							return card;
-						}
-					});
-					trigger.oldDialog = dialog;
-					trigger.newChoose = cards;
-					trigger.set("complexCard", true);
-					trigger.setContent("wyChooseCard");
 				}
 			}
+			if (allCard && hasBtn) {
+				event.oldDialog = dialog;
+				return true;
+			}
+			return false;
+		},
+		async content(event, trigger, player) {
+			const dialog = trigger.oldDialog;
+			const list = dialog.querySelectorAll(".buttons");
+			const caption = dialog.querySelector(".caption")?.textContent || "";
+			const description = dialog.querySelector(".text")?.textContent || "";
+			dialog.classList.add("removing");
+			dialog.close();
+			trigger.dialog = ui.create.dialog(caption + "\n" + description);
+			const cards2 = player.getCards("hs").forEach(c => c.classList.add("hidden", "wyremoving"));
+			const cards = list.map((btn, i) => {
+				const link = btn.link;
+				if (Array.isArray(link)) {
+					const card = game.createCard(link[2], "", "", link[3]);
+					if (card.node.suitnum) {
+						card.node.suitnum.remove();
+					} else {
+						card.node.info.remove();
+					}
+					card.storage.link = link;
+					return card;
+				} else {
+					const owner = get.owner(link);
+					const cardInfo = get.cardInfo(link);
+					const card = game.createCard(cardInfo[2], cardInfo[0], cardInfo[1], cardInfo[3]);
+					card.storage.link = link;
+					if (owner) {
+						ui.create.div(".gaintag", get.translation(owner), card);
+					}
+					if (btn.classList.contains("blank")) {
+						card.querySelector(".image").remove();
+						card.style.backgroundImage = "var(--cardback-url)";
+					}
+					return card;
+				}
+			});
+			trigger.newChoose = cards;
+			trigger.set("complexCard", true);
+			trigger.setContent("wyChooseCard");
 		},
 	},
 };
