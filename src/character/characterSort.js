88bacc9f29569f40d36c21ee1d8ffdc9a5f9dffb
xiyang141 <3495404507@qq.com>
2026-06-13 14:44:47 +0800

    'update'

--- src/character/characterSort.js
+++ src/character/characterSort.js
@@ -1,7 +1,7 @@
 const characterSort = {
   wyrg_noname: ["huamulan", "yue_caozhi", "dc_sb_zhugeliang", "dc_zhanggong", "huan_sunce", "huan_daqiao"],
   wyrg_rg: ["wyrg_player", "wyrg_sb"],
-  wyrg_diy: ["x_kelvdela"]
+  wyrg_diy: ["x_kelvdela", "wy_sunhanhua"]
 };
 export {
   characterSort
