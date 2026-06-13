07ea46d6f79ccb1fd5c8a2dc08a7295432eda0b7
xiyang141 <3495404507@qq.com>
2026-06-13 16:35:59 +0800

    update

--- src/sweet/content.ts
+++ src/sweet/content.ts
@@ -16,32 +16,49 @@ export const contents = {
 					card.classList.add("selectable");
 				}
 			}
-			if (!event.custom.replace) {
-				event.custom.replace = {};
-			}
 			if (event.custom.replace?.button) {
 				event.custom.replace.cardx = event.custom.replace.button;
 				delete event.custom.replace.button;
 			}
 			if (event.custom.add?.button) {
-				event.custom.add.card = event.custom.add.button;
+				event.custom.add.cardx = event.custom.add.button;
 				delete event.custom.add.button;
 			}
-			event.custom.replace.card = card => {
-				const event = get.event();
-				if (event.custom.replace.cardx) {
-					//@ts-ignore
-					event.custom.replace.cardx?.(card);
-				}
-				if (event.custom.add?.card) {
-					event.custom.add.card();
-				}
-				if (ui.selected.buttons.length > 0) {
+			if (event.custom.replace?.cardx) {
+				event.custom.replace.card = card => {
+					const event = get.event();
+					const cards = event.wy_custom.cards;
+					if (event.custom.replace.cardx) {
+						//@ts-ignore
+						event.custom.replace.cardx?.(card);
+					}
+					if (event.custom.add?.card) {
+						event.custom.add.card();
+					}
 					(ui.selected.cards as any) = ui.selected.buttons.slice();
-				} else {
-					(ui.selected.buttons as any) = ui.selected.cards.slice();
+					cards.forEach(card => {
+						if (card.classList.contains("selected")) {
+							card.updateTransform(true);
+						} else {
+							card.updateTransform();
+						}
+					});
+
+					game.check();
+				};
+			}
+			if (!event.custom.add) {
+				event.custom.add = {};
+			}
+			event.custom.add.card = () => {
+				const event = get.event();
+				if (event.custom.add.cardx) {
+					event.custom.add.cardx?.();
 				}
-				game.check();
+				if (event.custom.replace?.cardx) {
+					return;
+				}
+				(ui.selected.buttons as any) = ui.selected.cards.slice();
 			};
 			game.check();
 			game.pause();
